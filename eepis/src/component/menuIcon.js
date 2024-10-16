d4aaef33e7aef679c7e4825d863d540ac11d80fa
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-10-16 11:59:25 +0700

    rewrite with map

--- eepis/src/component/menuIcon.js
+++ eepis/src/component/menuIcon.js
@@ -1,41 +1,63 @@
 import '../App.css'
-import icon1 from '../img/pmb.png'
-import icon2 from '../img/gedung.png'
-import icon3 from '../img/elearning.png'
-import icon4 from '../img/innovation.png'
-import icon5 from '../img/job.png'
-import icon6 from '../img/nokorupsi.png'
+// import icon1 from '../img/pmb.png'
+// import icon2 from '../img/gedung.png'
+// import icon3 from '../img/elearning.png'
+// import icon4 from '../img/innovation.png'
+// import icon5 from '../img/job.png'
+// import icon6 from '../img/nokorupsi.png'
 import barisan from '../img/barisanlogo.png'
+
+let iconData = [
+    {
+        id: 1,
+        image: require('../img/pmb.png'),
+        text: "Penerimaan Mahasiswa Baru"
+    },
+
+    {
+        id: 2,
+        image: require('../img/job.png'),
+        text: "Job Arrengement System"
+    },
+    {
+        id: 3,
+        image: require('../img/gedung.png'),
+        text: "International Cooperation"
+    },
+    {
+        id: 4,
+        image: require('../img/elearning.png'),
+        text: "Layanan Elearning"
+    },
+    {
+        id: 5,
+        image: require('../img/innovation.png'),
+        text: "Innovation & collaboration"
+    },
+    {
+        id: 3,
+        image: require('../img/nokorupsi.png'),
+        text: "Zona berintegritas"
+    }
+]
 
 function Icon () {
     return(
         <div className="iconsection">
-            <div className="icon">
-                <div className="col">
-                    <img src={icon1} alt="img1"></img>
-                    <h3>Penerimaan<br></br>Mahasiswa Baru</h3>
-                </div>
-                <div className="col">
-                    <img src={icon5} alt="img2"></img>
-                    <h3>Job Arrengement<br></br>System</h3>
-                </div>
-                <div className="col">
-                    <img src={icon2} alt="img3"></img>
-                    <h3>International<br></br>Cooperation</h3>
-                </div>
-                <div className="col">
-                    <img src={icon3} alt="img4"></img>
-                    <h3>Layanan<br></br>Elearning</h3>
-                </div>
-                <div className="col">
-                    <img src={icon4} alt="img4"></img>
-                    <h3>Innovation &<br></br>Collaboration</h3>
-                </div>
-                <div className="col">
-                    <img src={icon6} alt="img4"></img>
-                    <h3>Zona<br></br>Intergritas</h3>
-                </div>
-            </div>
+
+            {
+                iconData.map(icon => {
+                    return(
+                        <div className="icon"  key={icon.id} style={iconStyle}>
+                            <div className="col">
+                                <img src={icon.image} alt="img1"></img>
+                                <h3>{icon.text}</h3>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            
             <img className="barislogo" src={barisan} alt="img"></img>
         </div>
         
@@ -43,3 +65,8 @@ function Icon () {
 }
 
 export default Icon
+
+const iconStyle = {
+    display: "flex",
+    justifyContent: "center"
+}
