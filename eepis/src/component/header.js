3f123aba78516a8b59a5413fa83f8028868f9aa5
Muhamad Daffa Al Ghifary <daffa.alghifary62@gmail.com>
2024-10-28 21:27:17 +0700

    navbar dropdown on progress

--- eepis/src/component/header.js
+++ eepis/src/component/header.js
@@ -1,44 +1,69 @@
 import logo from '../img/logo.png'
 import '../App.css'
-
+import React, { useState } from 'react';
 // function dropdown() {
 //     const menu = document.querySelectorAll(".menudrop")
 // }
 
 function Header() {
-    return(
-        <header className='navbar'>
-            <img src={logo} alt='' style={imgstyle}></img>
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
-            <nav>
-                <ul>
-                    <li><a href='#home'>BERANDA</a></li>
-                    <div className='drop'>
-                        <li id="trigger"><a href='#about'>TENTANG PENS</a></li>
-                        <div className='dropdown'>
-                            <ul className="menudrop">
-                                <li><a href='#PENS'>Tentang PENS</a></li>
-                                <li><a href='#PENS'>Visi Misi</a></li>
-                                <li><a href='#PENS'>Struktur Organisasi</a></li>
-                                <li><a href='#PENS'>Profil Direksi</a></li>
-                                <li><a href='#PENS'>Susunan Senat</a></li>
-                                <li><a href='#PENS'>Kalender Akademik</a></li>
-                                <li><a href='#PENS'>Hymne PENS</a></li>
-                                <li><a href='#PENS'>Peraturan Akademik</a></li>
-                                <li><a href='#PENS'>Pedoman Sistem Identitas</a></li>
-                                <li><a href='#PENS'>Akses ke PENS</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    
-                    <li><a href='#academic'>AKADEMIK</a></li>
-                    <li><a href='#services'>LAYANAN</a></li>
-                    <li><a href='#ppid'>PPID</a></li>
-                    <li><a href='#virtual'>VIRTUAL TOUR</a></li>
-                </ul>
-            </nav>
-        </header>
-    )
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <img src={logo} alt='logo' style={imgstyle}></img>
+      <ul className="navbar-menu">
+        <li className="navbar-item">BERANDA</li>
+        <li 
+          className="navbar-item dropdown" 
+          onMouseEnter={toggleDropdown} 
+          onMouseLeave={toggleDropdown}
+        >
+          TENTANG PENS
+          {dropdownOpen && (
+            <ul className="dropdown-menu">
+              <li className="dropdown-item">Tentang PENS</li>
+              <li className="dropdown-item">Visi Misi</li>
+              <li className="dropdown-item">Struktur Organisasi</li>
+              <li className="dropdown-item">Profil Direksi</li>
+              <li className="dropdown-item">Susunan Senat</li>
+              <li className="dropdown-item">Kalender Akademik</li>
+              <li className="dropdown-item">Hymne PENS</li>
+              <li className="dropdown-item">Peraturan Akademik</li>
+              <li className="dropdown-item">Pedoman sistem identitas</li>
+              <li className="dropdown-item">Akses ke PENS</li>
+            </ul>
+          )}
+        </li>
+
+        <li 
+          className="navbar-item dropdown" 
+          onMouseEnter={toggleDropdown} 
+          onMouseLeave={toggleDropdown}
+        >
+          AKADEMIK
+          {dropdownOpen && (
+            <ul className="dropdown-menu">
+              <li className="dropdown-item">Tentang PENS</li>
+              <li className="dropdown-item">Visi Misi</li>
+              <li className="dropdown-item">Struktur Organisasi</li>
+            </ul>
+          )}
+        </li>
+        <li className="navbar-item">LAYANAN</li>
+        <li className="navbar-item">PPID</li>
+        <li className="navbar-item">KONTAK</li>
+        <li className="navbar-item">VIRTUAL TOUR</li>
+        
+        
+        
+        <li className="navbar-item">CONTACT</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Header
